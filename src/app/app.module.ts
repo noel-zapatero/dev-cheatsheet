7947886a2b297a444147a438b837d3482c3cd11c
Noel Zapatero <witherwings77@gmail.com>
2026-05-21 14:28:43 -0300

    material mdc migration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,13 @@ import { LangContainerComponent } from './lang-container/lang-container.componen
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
+import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatDividerModule } from '@angular/material/divider';
 import { HomeContainerComponent } from './home-container/home-container.component';
 import { SoftwareContainerComponent } from './software-container/software-container.component';
